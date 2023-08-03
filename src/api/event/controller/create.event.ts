bf3e9adb547c9837fd8df82ff0198c77a8f96053
Aziz Khan <aziz@logicwind.com>
2023-08-03 14:35:06 +0530

    added validation

--- src/api/event/controller/create.event.ts
+++ src/api/event/controller/create.event.ts
@@ -4,9 +4,14 @@ import { httpStatusCodes } from "../../../utils/httpStatusCodes";
 import { CreateEventDTO } from "../../../schema/dto/event.dto";
 import { EventDAL } from "../../../schema/dal/event.dal";
 import EventMapper from "../event.mapper";
+import { validationResult } from "express-validator";
 
 const createEvent = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            throw new ApiError(result.array()[0].msg, httpStatusCodes.BAD_REQUEST);
+        }
         const { user } = req;
         if (!user || !user.id) throw new ApiError('Unauthorized', httpStatusCodes.UNAUTHENTICATED);
         const payload: CreateEventDTO = req.body;
